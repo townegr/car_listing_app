36dcf0fa43ece73968bcb22acc8dc6a2ef818240
Greg Towne <townegr@yahoo.com>
2014-04-14 09:47:25 -0400

    initial commit and update database.yml and gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarListing::Application.config.secret_key_base = '62569c5daba311c8c24fccfaafb14ec96c92143c86876851f99ff1d05110cb82eb271e335bfc61fab3110b25d3a2fa595a3a848b92017d7c3f2dc8dd8f7083d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarListing::Application.config.session_store :cookie_store, key: '_car_listing_session'
