e4e99b70351dfe7aba7d50caa0f8ec0459d25b6c
Greg Towne <townegr@yahoo.com>
2014-04-14 11:04:59 -0400

    update feature spec for car manufacturer

--- spec/features/user_records_manufacturer_spec.rb
+++ spec/features/user_records_manufacturer_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "record a car listing's manufacturer", %Q{
+feature "record a car manufacturer", %Q{
   As a car salesperson
   I want to record a car manufacturer
   So that I can keep track of the types of cars found in the lot
